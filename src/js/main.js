d6bf4b24469efe5046db3f02e6c46b41cf54963b
muhammadjon <muhammadjon@gmail.com>
2025-03-06 12:19:19 +0500

    Update product fetching logic and enhance card layout with pricing and quantity controls

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ import "./dark-mode.js";
 import { fetchData } from "./fetchData.js";
 import { showCards } from "./updateUI.js";
 
-fetchData("https://dummyjson.com/products")
+fetchData("https://dummyjson.com/products?limit=40")
   .then((data) => {
     showCards(data);
   })
