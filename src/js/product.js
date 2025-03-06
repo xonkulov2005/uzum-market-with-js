12a7f91fedf145e4b248fbcec352bdc48e0d262e
muhammadjon <muhammadjon@gmail.com>
2025-03-06 15:40:31 +0500

    Fix script path in product.html and refactor product rendering logic in product.js for improved clarity and maintainability

--- src/js/product.js
+++ src/js/product.js
@@ -13,42 +13,42 @@ fetchData(`https://dummyjson.com/product/${id}`)
     console.log(error);
   });
 
-// function renderProduct(product) {
-//   document.title = product.title;
+function renderProduct(product) {
+  document.title = product.title;
 
-//   const container = document.querySelector("div");
-//   container.className = "main-container py-10";
+  const container = document.querySelector("div");
+  container.className = "main-container py-10";
 
-//   container.innerHTML = `
-//   <div class="grid gap-8 md:grid-cols-2">
-//     <div class="carousel rounded-box">
-//       ${product.images
-//         .map(
-//           (img) => `
-//           <div class="carousel-item w-full">
-//             <img src="${img}" class=" object-cover" alt="${product.title}" />
-//           </div>
-//         `,
-//         )
-//         .join("")}
-//     </div>
+  container.innerHTML = `
+  <div class="grid gap-8 md:grid-cols-2">
+    <div class="carousel rounded-box">
+      ${product.images
+        .map(
+          (img) => `
+          <div class="carousel-item w-full">
+            <img src="${img}" class=" object-cover" alt="${product.title}" />
+          </div>
+        `,
+        )
+        .join("")}
+    </div>
 
-//     <div class="space-y-4">
-//       <h1 class="text-4xl font-bold">${product.title}</h1>
-//       <div class="badge badge-secondary">${product.brand}</div>
-//       <p class="text-2xl">$${product.price}</p>
-//       <p class="text-lg">${product.description}</p>
-//       <div class="flex items-center gap-2">
-//         <i class="fa-solid fa-star text-yellow-400"></i>
-//         <span>${product.rating}/5</span>
-//       </div>
-//       <button class="btn btn-primary">
-//         <i class="fa-solid fa-cart-plus"></i>
-//         Savatga qo'shish
-//       </button>
-//     </div>
-//   </div>
-// `;
+    <div class="space-y-4">
+      <h1 class="text-4xl font-bold">${product.title}</h1>
+      <div class="badge badge-secondary">${product.brand}</div>
+      <p class="text-2xl">$${product.price}</p>
+      <p class="text-lg">${product.description}</p>
+      <div class="flex items-center gap-2">
+        <i class="fa-solid fa-star text-yellow-400"></i>
+        <span>${product.rating}/5</span>
+      </div>
+      <button class="btn btn-primary">
+        <i class="fa-solid fa-cart-plus"></i>
+        Savatga qo'shish
+      </button>
+    </div>
+  </div>
+`;
 
-//   document.body.appendChild(container);
-// }
+  document.body.appendChild(container);
+}
