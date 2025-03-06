03e30ce6717eaffd96a69b8c66b05bfa2628785e
muhammadjon <muhammadjon@gmail.com>
2025-03-06 22:36:39 +0500

    Enhance score functionality in card component to update prices dynamically based on score changes

--- src/js/card.js
+++ src/js/card.js
@@ -3,19 +3,41 @@ import "./dark-mode.js";
 const plusBtns = document.querySelectorAll(".score-btn-plus");
 const minusBtns = document.querySelectorAll(".score-btn-minus");
 const scores = document.querySelectorAll(".score");
+const priceWithDiscounts = document.querySelectorAll(".price-with-discount");
+const prices = document.querySelectorAll(".price");
 
 plusBtns.forEach((btn, index) => {
   btn.addEventListener("click", () => {
     let currentScore = parseInt(scores[index].textContent);
-    scores[index].textContent = currentScore + 1;
+    let basePrice = parseFloat(prices[index].getAttribute("data-base"));
+    let baseDiscountPrice = parseFloat(
+      priceWithDiscounts[index].getAttribute("data-base"),
+    );
+
+    currentScore++;
+    scores[index].textContent = currentScore;
+
+    prices[index].textContent = `$${basePrice * currentScore}`;
+    priceWithDiscounts[index].textContent =
+      `$${baseDiscountPrice * currentScore}`;
   });
 });
 
 minusBtns.forEach((btn, index) => {
   btn.addEventListener("click", () => {
     let currentScore = parseInt(scores[index].textContent);
+    let basePrice = parseFloat(prices[index].getAttribute("data-base"));
+    let baseDiscountPrice = parseFloat(
+      priceWithDiscounts[index].getAttribute("data-base"),
+    );
+
     if (currentScore > 0) {
-      scores[index].textContent = currentScore - 1;
+      currentScore--;
+      scores[index].textContent = currentScore;
+
+      prices[index].textContent = `$${basePrice * currentScore}`;
+      priceWithDiscounts[index].textContent =
+        `$${baseDiscountPrice * currentScore}`;
     }
   });
 });
